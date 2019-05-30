c0ac8d7b8c70aa206bea0aafe0914f6fbffe626e
17034530 <17034530@myrp.edu.sg>
2019-05-30 15:01:22 +0800

    1

--- app/src/main/java/sg/edu/rp/c347/taskmanager/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c347/taskmanager/MainActivity.java
@@ -2,12 +2,37 @@ package sg.edu.rp.c347.taskmanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnAddTask;
+
+    ListView lvTask;
+    ArrayAdapter aaTask;
+    ArrayList<Task> task = new ArrayList<Task>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAddTask = findViewById(R.id.btnAddTask);
+        aaTask = new TaskAdapter(this, R.layout.activity_main, task);
+        lvTask = findViewById(R.id.lvTask);
+        aaTask = new TaskAdapter(this, R.layout.row, task);
+        lvTask.setAdapter(aaTask);
+
+        btnAddTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
