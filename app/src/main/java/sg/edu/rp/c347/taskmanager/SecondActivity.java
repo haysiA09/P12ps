d85ddaedf130be83a0096f82f6e47e86b3b37665
16022877 <16022877@myrp.edu.sg>
2019-05-30 15:56:47 +0800

    efwede

--- app/src/main/java/sg/edu/rp/c347/taskmanager/SecondActivity.java
+++ app/src/main/java/sg/edu/rp/c347/taskmanager/SecondActivity.java
@@ -1,5 +1,9 @@
 package sg.edu.rp.c347.taskmanager;
 
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,8 +13,10 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class SecondActivity extends AppCompatActivity {
+    int reqCode=12345;
 
     EditText etName, etDescription;
     Button btnAdd2, btnCancel;
@@ -36,6 +42,23 @@ public class SecondActivity extends AppCompatActivity {
                 db.insertTask(name, desc);
                 Log.d("Success" ,"New Task added");
                 db.close();
+
+                String[]info={name};
+                Calendar cal = Calendar.getInstance();
+                cal.add(Calendar.SECOND, 5);
+
+                Intent intent = new Intent(SecondActivity.this,
+                        ScheduledNotificationReceiver.class);
+                intent.putExtra("info",info);
+
+                PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                        SecondActivity.this, reqCode,
+                        intent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+                AlarmManager am = (AlarmManager)
+                        getSystemService(Activity.ALARM_SERVICE);
+                am.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(),
+                        pendingIntent);
                 finish();
             }
         });
